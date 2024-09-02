9af4b43ce6694f4843aee1a9195afa70dc7c49e7
Alejandro Rimasa <114910648+Ale-Rimasa@users.noreply.github.com>
2024-09-02 09:05:02 -0300

    Finalizado
    Se finaliza actividad sugerida, se agregan mas datos a las tablas para poder compararlos

--- Turno Noche 2do Cuatri 2024/Clase 4/Actividad Sugerida.sql
+++ Turno Noche 2do Cuatri 2024/Clase 4/Actividad Sugerida.sql
@@ -19,3 +19,65 @@ SELECT * FROM Actores WHERE ID_Pais NOT IN (1,2,3,6,7,8)
 SELECT TOP 1 * FROM Peliculas WHERE ID_Genero = 3 ORDER BY Fecha_Estreno DESC
 
 --F) Obtener la película que mayor recaudación haya obtenido en el año 2011.
+
+SELECT TOP 1 * FROM Peliculas WHERE YEAR(Fecha_Estreno) = 2011 ORDER BY Recaudacion DESC
+
+--G) Obtener las 10 mejores películas calificadas por los críticos. Mostrar todas las películas que se
+--encuentren en el 10° puesto si hay más de una película con igual puntaje en dicha posición.
+
+SELECT TOP 10 * FROM Peliculas ORDER BY Puntaje DESC		--PREGUNTAR
+
+--H) Obtener un listado de todas las películas en las que haya actuado el actor con código número 3.		
+
+SELECT * FROM Peliculas INNER JOIN
+Actores_X_Pelicula AS AXP
+	ON Peliculas.ID_Pelicula = AXP.ID_Pelicula
+	WHERE ID_Actores = 3
+
+--I) Obtener un listado que indique código de película, título y ganancia sólo de las películas que no hayan generado pérdida.
+SELECT * FROM Peliculas
+
+SELECT ID_Pelicula, Titulo, (Recaudacion - Inversion) AS [Ganancia]
+	FROM Peliculas WHERE Recaudacion >= Inversion
+
+--J) Obtener los datos de la película que menos duración tenga
+
+SELECT TOP 1 * FROM Peliculas Order BY Duracion
+
+--K) Obtener los datos de las películas que su título comience con la cadena 'Star'.
+
+SELECT * FROM Peliculas WHERE Titulo LIKE 'Star%'
+
+-- L) Obtener los datos de las películas que su título comience con la letra 'T' pero su última letra no sea 'A', 'E', 'I', 'O' ni 'U'.
+
+SELECT * FROM Peliculas WHERE Titulo LIKE 'T%' AND Titulo NOT LIKE '%[AEIOU]'
+
+--M) Obtener los datos de las películas que su título contenga al menos un número del 0 al 9.
+
+SELECT * FROM Peliculas WHERE Titulo LIKE '%[0-9]'
+
+--N) Obtener los datos de las películas que su título contenga exactamente cinco caracteres.
+--Resolverlo de dos maneras: 1) Utilizando el operador LIKE y comodines - 2) Utilizando la función
+--LEN.
+
+SELECT * FROM Peliculas WHERE Titulo LIKE '_____'
+
+SELECT * FROM Peliculas WHERE LEN(Titulo) = 5
+
+-- O) Obtener los datos de las películas cuya recaudación supere el 25% de la inversión.
+
+SELECT * FROM Peliculas WHERE Recaudacion > (Inversion * 1.25)
+
+--P ) Obtener el título y el valor promedio de cada ticket. Teniendo en cuenta que la recaudación es netamente sobre venta de tickets.
+
+SELECT Titulo, (Recaudacion / TicketsVendidos) AS [Valor Promedio] FROM Peliculas
+
+--Q) Obtener el título de la película, la recaudación en dólares y la recaudación pesos. Teniendo en cuenta que u$s 1 -> $ 4,39
+
+SELECT Titulo, (Recaudacion / 4.39) AS Dolares, (Recaudacion * 4.39) AS [Pesos] FROM Peliculas
+
+--R) Obtener los datos de las películas cuyo puntaje se encuentre en el intervalo (1, 7).
+
+SELECT * FROM Peliculas WHERE Puntaje >= 1 AND Puntaje <= 7
+
+SELECT * FROM Peliculas WHERE Puntaje BETWEEN 1 AND 7
